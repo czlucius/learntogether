6d0f45a70c45cbe6dff3d6dc0f86a8e6141dc0aa
Lucius Chee <lucius@czlucius.dev>
2024-01-15 16:09:00 +0800

    Registration basic UI

--- LearnTogether/app/src/main/java/com/sp/learntogether/ui/auth/RegisterFragment.java
+++ LearnTogether/app/src/main/java/com/sp/learntogether/ui/auth/RegisterFragment.java
@@ -2,29 +2,79 @@ package com.sp.learntogether.ui.auth;
 
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.PickVisualMediaRequest;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.sp.learntogether.R;
 import com.sp.learntogether.databinding.FragmentRegisterBinding;
 
 public class RegisterFragment extends Fragment {
 
     private FragmentRegisterBinding binding;
+    private RegisterViewModel vm;
+
+    private ActivityResultLauncher<PickVisualMediaRequest> pickMediaLauncher = registerForActivityResult(new ActivityResultContracts.PickVisualMedia(), uri -> {
+        if (uri == null) return;
+        vm.setImageUri(uri);
+    });
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentRegisterBinding.inflate(inflater, container, false);
+        vm = new ViewModelProvider(this)
+                .get(RegisterViewModel.class);
         return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        vm.getImageUri().observe(getViewLifecycleOwner(), uri -> {
+            if (uri == null) {
+                binding.profilePicRegister.setImageResource(R.drawable.baseline_add_24);
+            } else {
+                binding.profilePicRegister.setImageURI(uri);
+                // TODO face detection
+            }
+        });
+
+
+
+
+        ActivityResultContracts.PickVisualMedia.VisualMediaType mediaType = ActivityResultContracts.PickVisualMedia.ImageOnly.INSTANCE;
+        PickVisualMediaRequest request = new PickVisualMediaRequest.Builder()
+                .setMediaType(mediaType)
+                .build();
+        binding.profilePicRegister.setOnClickListener(v -> {
+            pickMediaLauncher.launch(request);
+        });
+        binding.profilePicRegister.setOnLongClickListener(v -> {
+            if (vm.getImageUri().getValue() != null) {
+                MaterialAlertDialogBuilder alertDialogBuilder = new MaterialAlertDialogBuilder(requireContext())
+                        .setTitle(R.string.remove_profile_picture)
+                        .setMessage(R.string.clear_pfp_notice)
+                        .setPositiveButton(R.string.confirm, (di, which) -> {
+                            vm.setImageUri(null);
+                        })
+                        .setNegativeButton(R.string.cancel, (di, which) -> { /* no-op */ });
+                alertDialogBuilder.show();
+            }
+            return true;
+        });
+
+
+
+
     }
 }
