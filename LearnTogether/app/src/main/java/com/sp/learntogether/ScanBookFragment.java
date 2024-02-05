ab8ae0b73846fa77d825ea3abd14a33331e99341
Lucius Chee <lucius@czlucius.dev>
2024-02-05 14:24:31 +0800

    Scanning books

--- LearnTogether/app/src/main/java/com/sp/learntogether/ScanBookFragment.java
+++ LearnTogether/app/src/main/java/com/sp/learntogether/ScanBookFragment.java
@@ -1,38 +1,186 @@
 package com.sp.learntogether;
 
-import androidx.lifecycle.ViewModelProvider;
-
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
+import android.provider.Settings;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.Preview;
+import androidx.camera.core.UseCase;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.snackbar.Snackbar;
 import com.sp.learntogether.databinding.FragmentScanBookBinding;
+import com.sp.learntogether.io.CameraInteractor;
+import com.sp.learntogether.objects.ScanWrapper;
+import com.sp.learntogether.ui.BookDisplayDialog;
+
+import java.io.IOException;
 
 public class ScanBookFragment extends Fragment {
+    private static final String CAMERA_PERMISSION = Manifest.permission.CAMERA;
 
-    private ScanBookViewModel mViewModel;
+    private ScanBookViewModel vm;
 
     public static ScanBookFragment newInstance() {
         return new ScanBookFragment();
     }
-
     private FragmentScanBookBinding binding;
+    private CameraInteractor cameraInteractor;
+
+
+    private ActivityResultLauncher<String> permissionRequestLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), granted -> {
+        if (granted) {
+            startCamera();
+        } else {
+            boolean showRationale = ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), CAMERA_PERMISSION);
+            if (showRationale) {
+                showPermissionsRationale();
+            } else {
+                // Permanent denial!
+                Snackbar sb = Snackbar.make(binding.getRoot(), R.string.permanent_denial_promt, Snackbar.LENGTH_SHORT);
+                sb.setAction(R.string.grant, v -> {
+                    launchAppInfo();
+                });
+            }
+        }
+    });
+
+    private void launchAppInfo() {
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        Uri pkg = Uri.fromParts("package", requireContext().getPackageName(), null);
+        intent.setData(pkg);
+        startActivity(intent);
+    }
+
+    private static final String TAG = "ScanBookFragment";
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         binding = FragmentScanBookBinding.inflate(inflater, container, false);
+        cameraInteractor = new CameraInteractor(requireContext());
+
+        vm = new ViewModelProvider(this).get(ScanBookViewModel.class);
+
+        vm.getDisplayedBook().observe(getViewLifecycleOwner(), book -> {
+            if (book != null) {
+                Log.i(TAG, "onCreateView: getDisplayedBook updated with non null value");
+                new BookDisplayDialog(requireContext(), book, newBook -> {
+                    try {
+                        vm.addBook(newBook);
+                    } catch (IOException e) {
+                        Snackbar.make(requireView(), R.string.error_adding_book, Snackbar.LENGTH_LONG).show();
+                    }
+                })
+                        .show();
+            }
+        });
         return binding.getRoot();
     }
+
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         // If the user comes here and do not grant permissions, gracefully degrade by only allowing manual entry.
+        reqPm();
+
+        vm.getScanned().observe(getViewLifecycleOwner(), scanWrappers -> {
+            for (ScanWrapper scanWrapper: scanWrappers) {
+                scanWrapper.display(view, isbn -> {
+                    if (isbn != null) {
+                        vm.loadBook(isbn);
+                    }
+                });
+            }
+        });
+
+        binding.enterIsbn.setOnClickListener(v -> {
+            EditText et = new EditText(requireContext());
+            et.setHint(R.string.isbn);
+            AlertDialog dialog = new AlertDialog.Builder(requireContext())
+                    .setTitle(R.string.enter_isbn)
+                    .setView(et)
+                    .setPositiveButton(R.string.check_info, (di, which) -> {
+                        vm.loadBook(et.getText().toString());
+                    }).setNegativeButton(R.string.cancel, (di, which) -> {})
+                    .show();
+
+        });
+
+
     }
+
+    private void reqPm() {
+        int granted = ContextCompat.checkSelfPermission(requireContext(), CAMERA_PERMISSION);
+        if (granted == PackageManager.PERMISSION_GRANTED) {
+            startCamera();
+        } else if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), CAMERA_PERMISSION)) {
+            // Show the rationale to the user
+            showPermissionsRationale();
+        } else {
+            requestCamera();
+        }
+    }
+
+    private CameraInteractor.UseCaseSupplier useCaseSupplier = (executor) -> {
+        // Use Case 1: Preview
+        Preview preview = new Preview.Builder().build();
+        if (binding != null) {
+            preview.setSurfaceProvider(binding.scanArea.getSurfaceProvider());
+        }
+
+        // Use case 2: Barcode analysis
+        ImageAnalysis imageAnalysis = new ImageAnalysis.Builder()
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build();
+
+        imageAnalysis.setAnalyzer(executor, vm.analyzer);
+
+        return new UseCase[]{preview, imageAnalysis};
+    };
+
+    private void startCamera() {
+        // Start the camera as all permissions have been successfully granted
+        cameraInteractor.start(useCaseSupplier, getViewLifecycleOwner(), e -> {
+            Snackbar.make(requireView(), R.string.failure_to_start_camera, Snackbar.LENGTH_LONG).show();
+        });
+    }
+
+    private void showPermissionsRationale() {
+        AlertDialog alertDialog = new MaterialAlertDialogBuilder(requireContext())
+                .setTitle(R.string.permissions_required)
+                .setMessage(R.string.camera_book_rationale)
+                .setPositiveButton(R.string.grant, (di, which) -> {
+                    requestCamera();
+                })
+                .setNegativeButton(R.string.cancel, (di, which) -> {
+                    // Degrade to manual entry
+                })
+                .create();
+        alertDialog.show();
+    }
+
+    private void requestCamera() {
+        permissionRequestLauncher.launch(CAMERA_PERMISSION);
+    }
+
+
 }
--- LearnTogether/app/src/main/java/com/sp/learntogether/ScanBookViewModel.java
+++ LearnTogether/app/src/main/java/com/sp/learntogether/ScanBookViewModel.java
@@ -1,7 +1,95 @@
 package com.sp.learntogether;
 
-import androidx.lifecycle.ViewModel;
+import android.app.Application;
+import android.util.Log;
 
-public class ScanBookViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.google.mlkit.vision.barcode.common.Barcode;
+import com.sp.learntogether.data.AppDatabase;
+import com.sp.learntogether.data.BookDao;
+import com.sp.learntogether.io.DatabaseInteractor;
+import com.sp.learntogether.models.Book;
+import com.sp.learntogether.objects.BarcodeAnalyzer;
+import com.sp.learntogether.objects.ScanWrapper;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ScanBookViewModel extends AndroidViewModel {
+
+    private static final String TAG = "ScanBookViewModel";
+    public BarcodeAnalyzer analyzer;
+    private final MutableLiveData<Set<ScanWrapper>> scanned = new MutableLiveData<>(new HashSet<>());
+    private final BookDao dao;
+
+    public ScanBookViewModel(Application application) {
+        super(application);
+        analyzer = new BarcodeAnalyzer(
+                this::scanBarcodes
+        );
+        dbIO = DatabaseInteractor.getInstance(application);
+        dao = AppDatabase.getInstance(application)
+                .bookDao();
+
+    }
+
+    public LiveData<Set<ScanWrapper>> getScanned() {
+        return scanned;
+    }
+
+    public void scanBarcodes(Iterable<Barcode> barcodes) {
+        boolean changed = false;
+        for (Barcode barcode: barcodes) {
+            assert scanned.getValue() != null;
+            ScanWrapper sw = new ScanWrapper(barcode, me -> {
+                if (scanned.getValue() != null) {
+                    scanned.getValue().remove(me);
+                }
+            });
+            if (sw.getIsbn() != null) {
+                scanned.getValue().add(sw);
+                changed = true;
+            }
+        }
+//        Log.i(TAG, "scanBarcodes: " + Arrays.toString(scanned.getValue().toArray()));
+
+        if (changed) {
+            // More codes detected
+            scanned.setValue(scanned.getValue());
+        }
+    }
+    private DatabaseInteractor dbIO;
+    private MutableLiveData<Book> displayedBook = new MutableLiveData<>();
+
+    public LiveData<Book> getDisplayedBook() {
+        return displayedBook;
+    }
+
+    public void addBook(Book book) throws IOException {
+        Log.i(TAG, "addBook: Inserting book" + book.imagePath);
+        if (book.imagePath != null && !book.imagePath.trim().isEmpty()) {
+            dbIO.persistBookImage(getApplication(), book, this::insertBook);
+        } else {
+            insertBook(book);
+        }
+
+    }
+
+    public void loadBook(String isbn) {
+        dbIO.queryOpenLibrary(isbn, book -> {
+            displayedBook.setValue(book);
+        });
+    }
+
+    public void clearBook() {
+        displayedBook.setValue(null);
+    }
+
+    private void insertBook(Book book1) {
+        new Thread(() -> dao.insertOne(book1)).start();
+    }
 }
