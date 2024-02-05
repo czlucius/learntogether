3f94eb534fff23de0c93884e983ff1a585620165
Lucius Chee <lucius@czlucius.dev>
2024-02-05 14:29:15 +0800

    Library and books

--- LearnTogether/app/src/main/java/com/sp/learntogether/ui/library/LibraryViewModel.java
+++ LearnTogether/app/src/main/java/com/sp/learntogether/ui/library/LibraryViewModel.java
@@ -1,7 +1,32 @@
 package com.sp.learntogether.ui.library;
 
-import androidx.lifecycle.ViewModel;
+import android.app.Application;
 
-public class LibraryViewModel extends ViewModel {
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModelProvider;
 
+import com.sp.learntogether.data.AppDatabase;
+import com.sp.learntogether.data.BookDao;
+import com.sp.learntogether.models.Book;
+
+import java.util.List;
+
+public class LibraryViewModel extends AndroidViewModel {
+    private BookDao dao;
+
+    public LibraryViewModel(@NonNull Application application) {
+        super(application);
+        AppDatabase db = AppDatabase.getInstance(application);
+        dao = db.bookDao();
+    }
+
+    public List<Book> getAllBooks() {
+        return dao.getAll();
+    }
+
+    public LiveData<List<Book>> allBooksLive() {
+        return dao.getAllLive();
+    }
 }
